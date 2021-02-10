eb0a41e9ec4838055c9144a89f2c1de3976a8520
Ndibechinonso <edwardndibe@gmail.com>
2021-02-10 12:36:16 +0100

    edit

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const convUnits = document.querySelector("#convUnits");
 const clearBtn = document.querySelector("#all-clear");
 const calcBtn = document.querySelector("#calc");
 const reverse = document.querySelector(".reverse")
-const converter = new Converter(resultDisplay);
 
+const converter = new Converter(resultDisplay, input);
 
-calcBtn.addEventListener("click", () => {
+function calculate() {
     converter.convertInput(input.value, unconvUnits.value, convUnits.value);
     if (input.value <= 0) return
     if (unconvUnits.value === convUnits.value) {
@@ -56,62 +56,18 @@ calcBtn.addEventListener("click", () => {
     else if (unconvUnits.value === "ml" && convUnits.value === "m") {
         result = input.value * 1609.344
     }
-    converter.diplayAnswer(result);
+    converter.diplayAnswer(result, input);
     resultDisplay.innerHTML = `${input.value} ${unconvUnits.value} = <br> ${result} ${convUnits.value}`
+}
 
+calcBtn.addEventListener("click", calculate);
 
-});
+reverse.addEventListener("click", () => {
+    const unconvUnitsVar = unconvUnits.value;
+    unconvUnits.value = convUnits.value;
+    convUnits.value = unconvUnitsVar;
+})
 
 clearBtn.addEventListener("click", () => {
     converter.clear()
-
 });
-
-reverse.addEventListener("click", () => {
-
-    if (input.value <= 0) return
-
-    if (unconvUnits.value === "km" && convUnits.value === "m") {
-
-        result = input.value / 1000
-    }
-    else if (unconvUnits.value === "km" && convUnits.value === "cm") {
-        result = input.value / 100000
-
-    }
-    else if (unconvUnits.value === "km" && convUnits.value === "ml") {
-        result = input.value / 0.621371192
-    }
-
-    else if (unconvUnits.value === "m" && convUnits.value === "km") {
-        result = input.value * 10000
-    }
-    else if (unconvUnits.value === "m" && convUnits.value === "cm") {
-        result = input.value / 100
-    }
-    else if (unconvUnits.value === "m" && convUnits.value === "ml") {
-        result = input.value / 0.000621371192
-    }
-
-    else if (unconvUnits.value === "cm" && convUnits.value === "km") {
-        result = input.value * 100000
-    }
-    else if (unconvUnits.value === "cm" && convUnits.value === "m") {
-        result = input.value * 100
-    }
-    else if (unconvUnits.value === "cm" && convUnits.value === "ml") {
-        result = input.value * 160934.4
-    }
-
-    else if (unconvUnits.value === "ml" && convUnits.value === "km") {
-        result = input.value / 1.609344
-    }
-    else if (unconvUnits.value === "ml" && convUnits.value === "cm") {
-        result = input.value / 160934.4
-    }
-    else if (unconvUnits.value === "ml" && convUnits.value === "m") {
-        result = input.value / 1609.344
-    }
-
-    resultDisplay.innerHTML = `${input.value} ${convUnits.value} = <br> ${result} ${unconvUnits.value}`
-})
